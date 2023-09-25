9bed932ebdbba7ceff401ee0cc89b73b1d157c4e
VoltaireNoir <mzahmed95@gmail.com>
2023-09-25 14:51:14 +0530

    Initial core trait prototype and example implementation

---
+++ src/brew.rs
@@ -0,0 +1,60 @@
+use super::{Error, Operation, PackError, Package, PackageManager};
+
+// Example implementation of HomeBrew package manager
+pub struct HomeBrew;
+
+impl HomeBrew {
+    const NAME: &'static str = "HomeBrew";
+    const CMD: &'static str = "brew";
+    const LIST_CMD: &'static str = "list";
+    const SEARCH_CMD: &'static str = "search";
+    const INSTALL_CMD: &'static str = "install";
+    const UNINSTALL_CMD: &'static str = "uninstall";
+    const UPDATE_CMD: &'static str = "update";
+}
+
+impl PackageManager for HomeBrew {
+    fn name(&self) -> &'static str {
+        Self::NAME
+    }
+
+    fn cmd(&self) -> &'static str {
+        Self::CMD
+    }
+
+    fn search(&self, pack: &str) -> Vec<Package> {
+        let out = self.execute_cmds(&[Self::SEARCH_CMD, pack]);
+        let outstr = std::str::from_utf8(&out.stdout).unwrap();
+        outstr
+            .lines()
+            .map(|s| Package {
+                name: s.into(),
+                version: None, // skip version for now
+            })
+            .collect()
+    }
+
+    fn execute(&self, pack: &Package, op: Operation) -> PackError<()> {
+        let cmd = match op {
+            Operation::Install => Self::INSTALL_CMD,
+            Operation::Uninstall => Self::UNINSTALL_CMD,
+            Operation::Update => Self::UPDATE_CMD,
+        };
+        self.execute_cmds_status(&[cmd, &pack.name])
+            .success()
+            .then_some(())
+            .ok_or(Error)
+    }
+
+    fn list_installed(&self) -> Vec<Package> {
+        let out = self.execute_cmds(&[Self::LIST_CMD]);
+        let outstr = std::str::from_utf8(&out.stdout).unwrap();
+        outstr
+            .lines()
+            .map(|s| Package {
+                name: s.into(),
+                version: None,
+            })
+            .collect()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,62 @@
+use std::process::{Command, ExitStatus, Output, Stdio};
 
+// Module containing example implementation of HomeBrew
+mod brew;
+
+// Primary interface. Multiple package managers can be grouped together as dyn PackageManager.
+trait PackageManager {
+    // Package manager name
+    fn name(&self) -> &'static str;
+
+    // Package manager command
+    fn cmd(&self) -> &'static str;
+
+    // Check if package manager is installed on system
+    fn is_installed(&self) -> bool {
+        Command::new(self.cmd())
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .spawn()
+            .is_ok()
+    }
+
+    // General package search
+    fn search(&self, pack: &str) -> Vec<Package>;
+
+    // List installed packages
+    fn list_installed(&self) -> Vec<Package>;
+
+    // Install, uninstall and update
+    fn execute(&self, pack: &Package, op: Operation) -> PackError<()>;
+
+    // Run arbitrary commands against the package manager and get output
+    fn execute_cmds(&self, cmds: &[&str]) -> Output {
+        // safe to unwrap when package manager is known to be available (see is_installed fn)
+        Command::new(self.cmd()).args(cmds).output().unwrap()
+    }
+
+    // Run arbitrary commands against the package manager and wait for ExitStatus
+    fn execute_cmds_status(&self, cmds: &[&str]) -> ExitStatus {
+        // safe to unwrap when package manager is known to be available (see is_installed fn)
+        Command::new(self.cmd()).args(cmds).status().unwrap()
+    }
+}
+
+struct Error;
+
+type PackError<T> = Result<T, Error>;
+
+#[derive(Debug)]
+struct Package {
+    name: String,
+    version: Option<Version>,
+}
+
+#[derive(Debug)]
+struct Version(u8, u8, u8);
+
+enum Operation {
+    Install,
+    Uninstall,
+    Update,
+}

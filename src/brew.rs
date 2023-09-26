b43a1a81d76ac818a111a71bc90d2fde92bcc8fe
VoltaireNoir <mzahmed95@gmail.com>
2023-09-26 15:42:53 +0530

    parse brew packages with version if present

--- src/brew.rs
+++ src/brew.rs
@@ -13,6 +13,13 @@ impl HomeBrew {
     const INSTALL_CMD: &'static str = "install";
     const UNINSTALL_CMD: &'static str = "uninstall";
     const UPDATE_CMD: &'static str = "update";
+
+    fn parse_package<'a, 'b>(line: &'a str) -> Package<'b> {
+        if let Some((name, version)) = line.split_once('@') {
+            return Package::from(name.trim().to_owned()).with_version(version.trim().to_owned());
+        }
+        Package::from(line.trim().to_owned())
+    }
 }
 
 impl PackageManager for HomeBrew {
@@ -27,10 +34,7 @@ impl PackageManager for HomeBrew {
     fn search(&self, pack: &str) -> Vec<Package> {
         let out = self.execute_cmds(&[Self::SEARCH_CMD, pack]);
         let outstr = std::str::from_utf8(&out.stdout).unwrap();
-        outstr
-            .lines()
-            .map(|s| Package::from(s.to_owned()))
-            .collect()
+        outstr.lines().map(|s| Self::parse_package(s)).collect()
     }
 
     fn execute_op(&self, pack: &Package, op: Operation) -> PackError<()> {
@@ -53,9 +57,6 @@ impl PackageManager for HomeBrew {
     fn list_installed(&self) -> Vec<Package> {
         let out = self.execute_cmds(&[Self::LIST_CMD]);
         let outstr = std::str::from_utf8(&out.stdout).unwrap();
-        outstr
-            .lines()
-            .map(|s| Package::from(s.to_owned()))
-            .collect()
+        outstr.lines().map(|s| Self::parse_package(s)).collect()
     }
 }

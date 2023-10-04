e7d10febdc3c91be0ffed598848adac682d18a86
VoltaireNoir <mzahmed95@gmail.com>
2023-10-04 16:28:11 +0530

    add basic integration tests

---
+++ tests/basic.rs
@@ -0,0 +1,40 @@
+use genpack::verify::Verify;
+use genpack::*;
+
+#[cfg(not(target_os = "windows"))]
+#[test]
+fn homebrew() {
+    let hb = managers::HomeBrew;
+    let hb = hb.verify().expect("HomeBrew not found in path");
+    // sync
+    assert!(hb.sync().success());
+    // search
+    assert!(hb.search("hello").iter().any(|p| p.name() == "hello"));
+    // install
+    assert!(hb.exec_op(&["hello".into()], Operation::Install).success());
+    // list
+    assert!(hb.list_installed().iter().any(|p| p.name() == "hello"));
+    // update
+    assert!(hb.exec_op(&["hello".into()], Operation::Update).success());
+    // update all
+    assert!(hb.update_all().success());
+    // uninstall
+    assert!(hb
+        .exec_op(&["hello".into()], Operation::Uninstall)
+        .success());
+    // TODO: Test AddRepo
+}
+
+#[cfg(target_os = "windows")]
+#[test]
+fn chocolatey() {
+    let choco = managers::Chocolatey;
+    let choco = choco.verify().expect("HomeBrew not found in path");
+    // sync
+    assert!(choco.sync().success());
+    // search
+    assert!(choco.search("hello").iter().any(|p| p.name() == "hello"));
+    // update all
+    assert!(choco.update_all().success());
+    // TODO: Test Install, Uninstall, Update, List and AddRepo
+}

8f43373f305d78b088a041b53f744d8829da3863
VoltaireNoir <mzahmed95@gmail.com>
2023-10-11 12:06:35 +0530

    add initial dnf implementation

--- src/managers.rs
+++ src/managers.rs
@@ -1,7 +1,9 @@
 mod apt;
 mod brew;
 mod choco;
+mod dnf;
 
 pub use apt::AdvancedPackageTool;
 pub use brew::HomeBrew;
 pub use choco::Chocolatey;
+pub use dnf::DandifiedYUM;
---
+++ src/managers/dnf.rs
@@ -0,0 +1,57 @@
+use crate::{Cmd, Commands, Package, PackageManager};
+use std::{fmt::Display, process::Command};
+
+#[derive(Debug)]
+pub struct DandifiedYUM;
+
+impl PackageManager for DandifiedYUM {
+    fn pkg_delimiter(&self) -> char {
+        '-'
+    }
+    fn parse_pkg<'a>(&self, line: &str) -> Option<Package<'a>> {
+        if let Some((pkg, _)) = line.split_once(':') {
+            return Some(pkg.trim().to_owned().into());
+        }
+        if line.contains(['@', '-']) {
+            let mut splt = line.split_whitespace();
+            let name = splt.next()?;
+            let ver = splt.next()?;
+            Some(Package::from(name.trim().to_owned()).with_version(ver.trim().to_owned()))
+        } else {
+            None
+        }
+    }
+}
+
+impl Display for DandifiedYUM {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("Dandified YUM (DNF)")
+    }
+}
+
+impl Commands for DandifiedYUM {
+    fn cmd(&self) -> Command {
+        Command::new("dnf")
+    }
+    fn get_cmds(&self, cmd: Cmd) -> &'static [&'static str] {
+        match cmd {
+            Cmd::Install => &["install"],
+            Cmd::Uninstall => &["remove"],
+            Cmd::Update => &["upgrade"],
+            Cmd::UpdateAll => &["distro-sync"],
+            Cmd::List => &["list"],
+            Cmd::Sync => &["makecache"],
+            Cmd::AddRepo => &["config-manager", "--add-repo"], // must come before repo
+            Cmd::Search => &["search"],
+        }
+    }
+
+    fn get_flags(&self, cmd: Cmd) -> &'static [&'static str] {
+        match cmd {
+            Cmd::Install | Cmd::Uninstall | Cmd::Update | Cmd::UpdateAll => &["--yes"],
+            Cmd::List => &["--installed"],
+            Cmd::Search => &["--all", "-q"],
+            _ => &[],
+        }
+    }
+}

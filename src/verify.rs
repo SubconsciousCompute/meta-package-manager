e83d1f82246b2407320470d59ef9fb93ae7fedbd
VoltaireNoir <mzahmed95@gmail.com>
2023-10-13 12:46:02 +0530

    update verify module docs

--- src/verify.rs
+++ src/verify.rs
@@ -1,3 +1,18 @@
+//! This module contains types that are used to signify that a package manager is installed / is in path and is safe to use.
+//!
+//! The [``crate::Commands``] trait (which the [``PackageManager``] trait relies on) internally uses `unwrap` on [``std::process::Command``] when it is executed to avoid
+//! extra error-handling on the user side. This is safe and won't cause a panic when the primary command of the given package maanger
+//! works (i.e. is in path), which is the assumption the API makes.
+//! However, the user cannot always be trusted to ensure this assumption holds true. Therefore, one can rely on the functionality provided by this
+//! module to check if a package manager is installed / is in path, and then construct a type that marks that it is now safe to use.
+//! This is done by leveraging the type system: the verified types can only be constructed using their provided constructor functions.
+//! They internally use the [``is_installed``] function and return `Some(Self)` only if it returns `true`.
+//!
+//! The type [``Verified``] is a generic wrapper that works with any `T` that implements the [``PackageManager``] trait.
+//! [``DynVerified``] is similar, except that it internally stores the type `T` as `Box<dyn PackageManager>` to allow dynamic dispatch with ease.
+//! These types can be constructed manaully by using their respective constructors or use the [``Verify``] trait, which provides a blanket implementation
+//! and lets you construct them directly from any instance of a type that implements [``PackageManager``].
+//! For example, `MyPackageMan::new().verify()` or `MyPackageMan::new().verify_dyn()`.
 use std::ops::Deref;
 
 use crate::PackageManager;
@@ -58,6 +73,9 @@ impl Display for DynVerified {
 }
 
 /// Check if package manager is installed on the system
+///
+/// The current implementation merely runs the primary package-manager command
+/// and checks if it returns an error or not.
 pub fn is_installed<P: PackageManager + ?Sized>(pm: &P) -> bool {
     pm.cmd()
         .stdout(Stdio::null())

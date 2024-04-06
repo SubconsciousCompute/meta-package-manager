859961a5f935c6fe2edbaebf3a94355db34929ce
Dilawar Singh <dilawar@subcom.tech>
2024-04-06 14:23:53 +0530

    update readme (#30)
    * chore: add a simple example
    
    * doc: Updates README.md

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,16 +1,3 @@
-# Meta Package Manager
-
-A meta package manager for interfacing with multiple distro/platform-specific
-package managers using a single, simple, unified interface. `mpm` is both a
-Rust library and a CLI utility.
-
-It is inspired by Python's
-[meta-package-manager](https://github.com/kdeldycke/meta-package-manager) which is far
-ahead in functionality.
-
-## Basic Usage
-
-```rust
 /// use trait
 use mpm::PackageManager;
 use mpm::{MetaPackageManager, Operation};
@@ -49,13 +36,3 @@ fn main() -> anyhow::Result<()> {
     }
     Ok(())
 }
-```
-
-# Command-line Interface
-
-The CLI provides a common interface to execute operations using different
-package managers. It automatically detects the package managers available on the
-system and picks one of them to perform operations by default (the user can also
-target a specific package manager if required).
-
-Run `mpm --help` for more details.

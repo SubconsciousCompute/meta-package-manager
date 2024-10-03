7731456107c758f13f575f27e6039c7a97059b94
समीर सिंह Sameer Singh <lumarzeli30@gmail.com>
2024-10-03 22:31:57 +0530

    feat: elevate to root only for specific commands

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,6 @@ use clap::Parser;
 use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 fn main() {
-    // elevate to sudo
-    #[cfg(target_os = "linux")]
-    if let Err(e) = sudo::with_env(&["CARGO_", "RUST_LOG"]) {
-        tracing::warn!("Failed to elevate to sudo: {e}.");
-    }
-
     tracing_subscriber::registry()
         .with(fmt::layer())
         .with(EnvFilter::from_default_env())
